514f4d5ffc986a611a074922290735b90955dd81
ericjwittrock <ericjwittrock@gmail.com>
2023-12-02 22:29:03 -0600

    about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,26 @@
 function About() {
-    return (
+  return (
+    <div className="text-white">
+      <div className="aboutTitle" width="100%" style={{paddingTop: "2%"}}>
         <h1>About</h1>
-    )
+        <hr
+          style={{
+            color: "white",
+             backgroundColor: "whitesmoke",
+            height: 10,
+          }}
+        />
+      </div>
+
+      <h2>SE/ComS319 Construction of User Interfaces, Fall 2023</h2>
+      <div className="text-light" style={{paddingTop: "5%"}}>
+        <h4>3 December 2023</h4>
+        <p>Eric Wittrock                        ejw3@iastate.edu</p>
+        <p>Kale Kester                          kmkester@iastate.edu</p>
+      </div>
+      <p style={{paddingTop: "10%"}}>Dr. Abraham N. Aldaco Gastelum       aaldaco@iastate.edu</p>
+    </div>
+  );
 }
 
 export default About;
